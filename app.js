c9b06271d35bd2dd6c9e9960735d06b3c8efe088
Jain K <kuriakosejain456@gmail.com>
2023-12-28 21:06:42 +0530

    fixed back tic bug

--- app.js
+++ app.js
@@ -30,5 +30,5 @@ app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> {
-    console.log('server is running on port ${PORT}');
+    console.log(`server is running on port ${PORT}`);
 })
