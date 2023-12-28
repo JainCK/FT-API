240010beacb4b5bb96b6a0e2d981a7330413c3f7
Jain K <kuriakosejain456@gmail.com>
2023-12-28 17:22:09 +0530

    fileSchema and export creeated

--- src/models/File.js
+++ src/models/File.js
@@ -1,2 +1,14 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
+
+const fileSchema = new Schema({
+    filename: { type: String, required: true },
+    size: { type: Number, required: true },
+    type: { type: String, required: true },
+    uploadDate: { type: Date, default: Date.now},
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+});
+
+const File = mongoose.model('File', fileSchema);
+
+module.exports = File;
