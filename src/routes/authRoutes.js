74b85b0c433a37868f2143ba73e9dc57b14f419b
Jain K <kuriakosejain456@gmail.com>
2023-12-28 17:34:00 +0530

    added authRoutes file for auth

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const zod = require('zod');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/User');
