03346d4661a6d27f5a188c187144ae535acb1be1
Jain K <kuriakosejain456@gmail.com>
2023-12-28 17:07:52 +0530

    added server port

--- app.js
+++ app.js
@@ -14,3 +14,15 @@ mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
+app.get('/', (req, res) => {
+    res.send('FS & Collab API');
+});
+
+const authRoutes = require ('./src/routes/authRoutes');
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=> {
+    console.log('server is running on port ${PORT}');
+})
