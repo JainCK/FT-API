eafe277a845d16f1fba6867b445aab2e2b71eab4
Jain K <kuriakosejain456@gmail.com>
2023-12-28 17:39:46 +0530

    added registaration schema with zod

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,3 +5,9 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 const User = require('../models/User');
+
+const registerSchema = zod.object({
+    username: zod.string().min(3),
+    email: zod.string().email(),
+    password: zod.string().min(6),
+})
