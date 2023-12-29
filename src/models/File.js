004eb8439047df4f31ec55c9a0faf09d2592941e
Jain K <kuriakosejain456@gmail.com>
2023-12-29 20:55:20 +0530

    rewrote the schema

--- src/models/File.js
+++ src/models/File.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
 
-const fileSchema = new Schema({
-    filename: { type: String, required: true },
-    size: { type: Number, required: true },
-    type: { type: String, required: true },
-    uploadDate: { type: Date, default: Date.now},
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+const fileSchema = new mongoose.Schema({
+  filename: { type: String, required: true },
+  size: { type: Number, required: true },
+  type: { type: String, required: true },
 });
 
 const File = mongoose.model('File', fileSchema);
