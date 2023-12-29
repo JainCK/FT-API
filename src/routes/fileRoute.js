9747c87da1802b5d7c30512d54acca50c9369838
Jain K <kuriakosejain456@gmail.com>
2023-12-29 15:19:23 +0530

    added multer package and fileRoute file

---
+++ src/routes/fileRoute.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const grid = require('gridfs-stream');
+const multer = require('multer');
