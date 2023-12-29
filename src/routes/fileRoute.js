d2947ffa79c98a61e68d82fee9ec6b0bad7bbe41
Jain K <kuriakosejain456@gmail.com>
2023-12-29 15:41:02 +0530

    download method added

--- src/routes/fileRoute.js
+++ src/routes/fileRoute.js
@@ -6,6 +6,7 @@ const multer = require('multer');
 
 
 const File = require('../models/File');
+const readstream = require('gridfs-stream/lib/readstream');
 
 let gfs;
 mongoose.connection.once('open', () => {
@@ -16,6 +17,9 @@ mongoose.connection.once('open', () => {
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
+
+
+//upload
 
 router.post('/upload', upload.single('file'), async (req, res) => {
     try {
@@ -48,6 +52,7 @@ router.post('/upload', upload.single('file'), async (req, res) => {
     }
 })
 
+//retrival
 
 router.get('/:fileId', async (req, res) => {
     try {
@@ -65,4 +70,32 @@ router.get('/:fileId', async (req, res) => {
         });
     }
 });
+
+//download
+
+router.get('/:fileId/content', async (req, res) => {
+    try {
+        const File = await File.findById(req.params.fileId);
+        if(!File){
+            return res.status(404).json({
+                error: 'File not found'
+            });
+        }
+
+        const readstream = gfs.createReadStream({
+            filename: file._id.toString(),
+        });
+
+
+        readstream.pipe(res);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: 'Internal Error'
+        });
+    }
+});
+
+
+
 
