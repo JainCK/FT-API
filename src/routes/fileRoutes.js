a4894181c4a25d22d9e983974f94a85e1a2fb13b
Jain K <kuriakosejain456@gmail.com>
2023-12-29 20:55:51 +0530

    chnaged back to post req, tested in postman

--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -1,31 +1,29 @@
-// src/routes/fileRoutes.js
-
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const grid = require('gridfs-stream');
-const crypto = require('crypto');
-const path = require('path');
 const multer = require('multer');
 
-const File = require('../models/File');
-
-// Initialize GridFS
-let gfs;
-mongoose.connection.once('open', () => {
-  gfs = grid(mongoose.connection.db, mongoose.mongo);
-  gfs.collection('uploads');
-});
-
-// Set up multer storage
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-// Upload a file
-router.post('/upload', async (req, res) => {
-  res.json({message: 'hello' })
+const fileRoutes = (gfs) => {
+router.post('/upload', upload.single('file'), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file uploaded' });
+    }
+
+    const buffer = req.file.buffer;
+    
+
+    res.status(201).json({
+      message: 'File uploaded successfully',
+    });
+  } catch (error) {
+    console.error('File upload error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
+return router;
+};
 
-
-
-module.exports = router;
+module.exports = fileRoutes;
