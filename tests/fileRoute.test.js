d4c3950990df69723ecdada25fb8aa32eac82a79
Jain K <kuriakosejain456@gmail.com>
2023-12-30 16:57:58 +0530

    creating test files with jest

--- tests/fileRoute.test.js
+++ tests/fileRoute.test.js
@@ -1,26 +1,2 @@
-const express = require('express');
-const router = express.Router();
-const multer = require('multer');
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
-router.post('/upload', upload.single('file'), async (req, res) => {
-  try {
-    if (!req.file) {
-      return res.status(400).json({ error: 'No file uploaded' });
-    }
-
-    const buffer = req.file.buffer;
-    // You can add more logic here to handle the file
-
-    res.status(201).json({
-      message: 'File uploaded successfully',
-    });
-  } catch (error) {
-    console.error('File upload error:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-module.exports = router;
+const request = require('supertest');
+const app = require('./path-to-your-app');
