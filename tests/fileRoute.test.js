15b2f4b9d1359567f568b47118c8d70802f4d7ac
Jain K <kuriakosejain456@gmail.com>
2023-12-30 16:59:21 +0530

    responses done

--- tests/fileRoute.test.js
+++ tests/fileRoute.test.js
@@ -6,5 +6,9 @@ describe('File Routes', () => {
     const response = await request(app)
       .post('/api/files/upload')
       .attach('file', '../test.txt');
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toHaveProperty('message', 'File uploaded successfully');
+    expect(response.body).toHaveProperty('fileId');
     });
   });
