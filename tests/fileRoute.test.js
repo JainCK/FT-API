37bd200e41869e54659722f79c626971737c3dca
Jain K <kuriakosejain456@gmail.com>
2023-12-30 16:58:41 +0530

    describe done

--- tests/fileRoute.test.js
+++ tests/fileRoute.test.js
@@ -1,2 +1,10 @@
 const request = require('supertest');
 const app = require('./path-to-your-app');
+
+describe('File Routes', () => {
+  it('should upload a file', async () => {
+    const response = await request(app)
+      .post('/api/files/upload')
+      .attach('file', '../test.txt');
+    });
+  });
